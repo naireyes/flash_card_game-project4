09240dc296cd363954321a367a797fd1a227c4de
Naira_Reyes <gab1ana1@gmail.com>
2020-07-07 16:17:38 -0400

    Started on pseudocode

---
+++ script.js
@@ -0,0 +1,24 @@
+let questions = "";
+let answers = "";
+let score = "";
+
+
+function start() {
+    //It will restart the game
+}
+
+function reset() {
+    //It will reset
+}
+
+function score() {
+
+}
+
+function questions() {
+
+}
+
+function answer() {
+
+}
