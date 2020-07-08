acf3d767d0e7407fff1ae3068b0940a926629515
Naira_Reyes <gab1ana1@gmail.com>
2020-07-08 12:08:58 -0400

    Revised questions

--- script.js
+++ script.js
@@ -1,63 +1,64 @@
-class Questions {
-    constructor(questions, choices, correctAnswer) {
-        this.questions = questions;
-        this.choices = [{}];
-        this.correctAnswer = correctAnswer;
+const question = document.querySelector(".questions");
+const choiceA = document.querySelector(".a");
+const choiceB = document.querySelector(".b");
+const choiceC = document.querySelector(".c");
+const choiceD = document.querySelector(".d");
+const start = document.querySelector('.start');
+const newGame = document.querySelector('.newGame');
+
+
+let qNA = [{
+        question: "What is the name for the Jewish New Year?",
+        choiceA: "a.) Hannukah",
+        choiceB: "b.) Yum Kippur",
+        ChoiceC: "c.) Kwanza",
+        ChoiceD: "d.) Rosh Hashanah",
+        correctAnswer: "d"
+    },
+    {
+        question: "What was the name of the band Lionel Richie was part of?",
+        ChoiceA: "a.) King Harvest",
+        ChoiceB: "b.) Spectrums",
+        ChoiceC: "c.) Commodores",
+        ChoiceD: "d.) The Marshall Tucker Band",
+        correctAnswer: "c"
+    },
+    {
+        question: "Which one of these characters is not friends with Harry Potter?",
+        ChoiceA: "Ron Weasley",
+        ChoiceB: "Neville Longbottom",
+        ChoiceC: "Draco Malfoy",
+        ChoiceD: "Hermione Granger",
+        correctAnswer: "c"
     }
-    startQuestions() {
-
-    }
-    pushChoices(choices) {
-
-    }
-
-}
-myQuestions1 = new Questions("What is the name for the Jewish New Year?", [{
-    a: "Hannukah",
-    b: "Yum Kippur",
-    c: "Kwanza",
-    d: "Rosh Hashanah"
-}], "d")
-myQuestions2 = new Questions("How many blue stripes are there on the U.S. flag?", [{
-    a: 6,
-    b: 7,
-    c: 13,
-    d: 1
-}], "c")
-myQuestions3 = new Questions("Which one of these characters is not friends with Harry Potter?", [{
-    a: "Ron Weasley",
-    b: "Neville Longbottom",
-    c: "Draco Malfoy",
-    d: "Hermione Granger"
-}], "c")
-
-console.log(myQuestions1);
+];
+console.log(qNA[0]);
 
 
-function start() {
-    //addEventlistener to start button
-    let startButton = document.querySelector('.start');
-    startButton.addEventListener('click', questions())
-}
+// function start() {
+//     //addEventlistener to start button
+//     let startButton = document.querySelector('.start');
+//     startButton.addEventListener('click', Questions.startQuestions())
+// }
 
 
 
-function reset() {
-    //This will run the start function
-};
+// function reset() {
+//     //This will run the start function
+// };
 
-function questions() {
-    //When start button is pressed start with question 1
-    this.questions;
+// function questions() {
+//     //When start button is pressed start with question 1
+//     this.questions;
 
-};
+// };
 
-function answer() {
-    //get input from user for answer, if answer is not correctAnswer, run wrong answer function, 
-    // if correct answer is guessed, go to the next question
+// function answer() {
+//     //get input from user for answer, if answer is not correctAnswer, run wrong answer function, 
+//     // if correct answer is guessed, go to the next question
 
 
-};
+// };
 
-function wrongAnswer() {}
-//this will turn on sound 
+// function wrongAnswer() {}
+// //this will turn on sound 
