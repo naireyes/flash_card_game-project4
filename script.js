17e4d1dcbbfdafb33950c5715ef8e7752153c22a
Naira_Reyes <gab1ana1@gmail.com>
2020-07-08 10:22:01 -0400

    Created Questions class with choices and answers

--- script.js
+++ script.js
@@ -1,46 +1,63 @@
-const questions = [{
-        question: "What is the name for the Jewish New Year?",
-        choices: {
-            a: "Hannukah",
-            b: "Yum Kippur",
-            c: "Kwanza",
-            d: "Rosh Hashanah"
-        },
-        correctAnswer: "d"
-    },
-    {
-        question: "How many blue stripes are there on the U.S. flag?",
-        choices: {
-            a: 6,
-            b: 7,
-            c: 13,
-            d: 1
-        },
-        {}
+class Questions {
+    constructor() {
+        this.questions = questions;
+        this.choices = [];
+        this.correctAnswer = correctAnswer;
     }
-];
-let score = "";
+    pushChoices()
+
+}
+
+myQuestions = new Questions1("What is the name for the Jewish New Year?", [{
+    a: "Hannukah",
+    b: "Yum Kippur",
+    c: "Kwanza",
+    d: "Rosh Hashanah"
+}], "d")
+myQuestions = new Questions2("How many blue stripes are there on the U.S. flag?", [{
+    a: 6,
+    b: 7,
+    c: 13,
+    d: 1
+}], c)
+myQuestions = new Questions3("Which one of these characters is not friends with Harry Potter?", [{
+    a: "Ron Weasley",
+    b: "Neville Longbottom",
+    c: "Draco Malfoy",
+    d: "Hermione Granger"
+}], c)
 
 
 function start() {
-    //It will restart the game
-}
+    //addEventlistener to start button
+    let startButton = document.querySelector('.start');
+    startButton.addEventListener('click', questions())
 
-function reset() {
-    //It will reset
-}
 
-function score() {
 
-}
+    function reset() {
+        //This will run the start function
+    }
 
-function questions() {
+    function questions() {
+        //When start button is pressed start with question 1
 
-}
+    }
 
-function answer() {
+    function answer() {
+        //get input from user for answer, if answer is not correctAnswer, run wrong answer function, 
+        // if correct answer is guessed, go to the next question
 
-}
+    }
 
-//If answer is correct **
-//if answer is wrong **
+    function wrongAnswer()
+    //this will turn on sound 
+
+    function score() {
+        //if correct answer = correctAnswer run score function
+    }
+
+
+
+    //If answer is correct **
+    //if answer is wrong **
